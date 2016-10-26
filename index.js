e5686f6fcc25453be3956dab580e7ae72ab6deec
Brad Hanebury <brad@mobials.com>
2016-10-26 13:15:54 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+
+const uuid = require('uuid');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+/**
+ * Begin the Mobials JS SDK
+ */
+var MobialsAPI = {};
+
+//we need to fetch our global MobialsAPI variable from wherever this was called.
+(function (global){
+
+    MobialsAPI = typeof global.MobialsAPI !== 'undefined' ? global.MobialsAPI : MobialsAPI;
+    MobialsAPI.debug = typeof MobialsAPI.debug === 'undefined' ? false : MobialsAPI.debug;
+    MobialsAPI.dispatch_uri = MobialsAPI.dispatch_uri ? MobialsAPI.dispatch_uri : 'https://api.mobials.com/tracker/dispatch';
+    MobialsAPI.api_uri = MobialsAPI.api_uri ? MobialsAPI.api_uri : 'https://api.mobials.com/api/js';
+    MobialsAPI.APIKey = MobialsAPI.APIKey ? MobialsAPI.APIKey : null;
+    MobialsAPI.domain = MobialsAPI.domain ? MobialsAPI.domain : 'static.mobials.com';
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {});
+
+
+/**
+ *
+ * @param businessId
+ * @param callback a callback function
+ */
+MobialsAPI.fetchRating = function(businessId, callback) {
+    var http = new XMLHttpRequest();
+
+    http.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            callback(this.responseText);
+        }
+    };
+
+    http.open("GET", MobialsAPI.api_uri + '/business/' + businessId + '/rating?access_token=' + MobialsAPI.APIKey, true);
+    http.setRequestHeader("Content-type", "application/json");
+    http.send();
+};
+
+/**
+ *
+ * @param businessIds array of business ids
+ * @param callback a callback function
+ */
+MobialsAPI.fetchBatchRatings = function(businessIds, callback) {
+
+    if (businessIds.length > 100) {
+        throw "Maximum 100 businesses at a time";
+    }
+
+    var http = new XMLHttpRequest();
+
+    http.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            callback(this.responseText);
+        }
+    };
+
+    var url = MobialsAPI.api_uri + '/businesses/ratings?access_token=' + MobialsAPI.APIKey + '&business_ids=';
+    url += businessIds.join(',');
+
+    http.open("GET", url, true);
+    http.setRequestHeader("Content-type", "application/json");
+    http.send();
+};
+
+MobialsAPI.fetchBatchRatings([1,2,3,4,5], function(response) {
+    console.log(response);
+});
+
+module.exports = MobialsAPI;
